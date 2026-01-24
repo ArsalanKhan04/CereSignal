56767d18da0da71bf37d249af16258ae9734c43a
AtharRizwan <atharrizwan234@gmail.com>
2026-01-24 20:41:32 +0500

    parameterized neurogate

--- backend/external/models/neurogate.py
+++ backend/external/models/neurogate.py
@@ -2,11 +2,18 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
+
 class WaveLayer(nn.Module):
     def __init__(self, in_channels, kernel_size, dilation):
         super(WaveLayer, self).__init__()
         self.padding = (kernel_size - 1) * dilation
-        self.conv = nn.Conv1d(in_channels, in_channels, kernel_size, padding=self.padding, dilation=dilation)
+        self.conv = nn.Conv1d(
+            in_channels,
+            in_channels,
+            kernel_size,
+            padding=self.padding,
+            dilation=dilation,
+        )
         self.tanh = nn.Tanh()
         self.sig = nn.Sigmoid()
         self.filter = nn.Conv1d(in_channels, in_channels, 1)
@@ -18,9 +25,10 @@ class WaveLayer(nn.Module):
         torch.nn.init.xavier_uniform_(self.filter.weight, gain=1.0)
         torch.nn.init.xavier_uniform_(self.gate.weight, gain=1.0)
         torch.nn.init.xavier_uniform_(self.conv2.weight, gain=1.0)
-       # self.skip = nn.Conv1d(out_channels, in_channels, 1)
-       # self.residual = nn.Conv1d(out_channels, in_channels, 1)
-        
+
+    # self.skip = nn.Conv1d(out_channels, in_channels, 1)
+    # self.residual = nn.Conv1d(out_channels, in_channels, 1)
+
     def forward(self, x):
         # x_padded = torch.nn.functional.pad(x, (self.padding, 0))
         output = self.conv(x)
@@ -28,11 +36,12 @@ class WaveLayer(nn.Module):
         gate = self.gate(output)
         tanh = self.tanh(filter)
         sig = self.sig(gate)
-        z = tanh*sig
-        z = z[:,:,:-self.padding]
+        z = tanh * sig
+        z = z[:, :, : -self.padding]
         z = self.conv2(z)
         x = x + z
         return x
+
 
 class WaveBlock(nn.Module):
     def __init__(self, in_channels, out_channels, kernel_size, dilation_rates):
@@ -50,12 +59,17 @@ class WaveBlock(nn.Module):
             x = layer(x)
         return x
 
+
 class MFFMBlock(nn.Module):
     def __init__(self, in_channels):
         super(MFFMBlock, self).__init__()
-        self.conv1 = nn.Conv1d(in_channels=in_channels, out_channels=8, kernel_size=5, padding=2)
+        self.conv1 = nn.Conv1d(
+            in_channels=in_channels, out_channels=8, kernel_size=5, padding=2
+        )
         self.bn1 = nn.BatchNorm1d(8)
-        self.conv2 = nn.Conv1d(in_channels=in_channels + 8, out_channels=16, kernel_size=5, padding=2)
+        self.conv2 = nn.Conv1d(
+            in_channels=in_channels + 8, out_channels=16, kernel_size=5, padding=2
+        )
         self.bn2 = nn.BatchNorm1d(16)
 
         nn.init.xavier_uniform_(self.conv1.weight)
@@ -67,22 +81,31 @@ class MFFMBlock(nn.Module):
         x2 = F.relu(self.bn2(self.conv2(x1)))
         return torch.cat((x2, x1), dim=1)
 
+
 class NeuroGate(nn.Module):
-    def __init__(self):
+    def __init__(self, n_chans=22):
         super(NeuroGate, self).__init__()
-        self.mffm_block1 = MFFMBlock(44)
-        self.wave_block1 = WaveBlock(44, 68, 3, 8)
+        self.mffm_block1 = MFFMBlock(n_chans * 2)
+        self.wave_block1 = WaveBlock(n_chans * 2, n_chans * 2 + 24, 3, 8)
         self.mffm_block2 = MFFMBlock(20)
         self.wave_block2 = WaveBlock(20, 20 + 24, 3, 8)
         self.mffm_block3 = MFFMBlock(20)
-        self.conv1 = nn.Conv1d(in_channels=68, out_channels=20, kernel_size=3, padding=1)
-        self.bn1 = nn.BatchNorm1d(44)
-        self.conv2 = nn.Conv1d(in_channels=20 + 24, out_channels=20, kernel_size=3, padding=1)
+        self.conv1 = nn.Conv1d(
+            in_channels=n_chans * 2 + 24, out_channels=20, kernel_size=3, padding=1
+        )
+        self.bn1 = nn.BatchNorm1d(n_chans * 2)
+        self.conv2 = nn.Conv1d(
+            in_channels=20 + 24, out_channels=20, kernel_size=3, padding=1
+        )
         self.bn2 = nn.BatchNorm1d(20)
-        self.conv3 = nn.Conv1d(in_channels=20 + 24, out_channels=20, kernel_size=3, padding=1)
+        self.conv3 = nn.Conv1d(
+            in_channels=20 + 24, out_channels=20, kernel_size=3, padding=1
+        )
         self.bn3 = nn.BatchNorm1d(20)
         self.bn4 = nn.BatchNorm1d(20)
-        self.encoder = nn.TransformerEncoder(nn.TransformerEncoderLayer(20, 4, dropout=0.5, batch_first=True), 2)
+        self.encoder = nn.TransformerEncoder(
+            nn.TransformerEncoderLayer(20, 4, dropout=0.5, batch_first=True), 2
+        )
         self.fc = nn.Linear(20, 2)
 
         nn.init.xavier_uniform_(self.conv1.weight)
@@ -125,11 +148,11 @@ class NeuroGate(nn.Module):
         x2 = self.wave_block1(x)
         x = x1 + x2
 
-        #Apply spatial dropout
+        # Apply spatial dropout
         # x = x.permute(0, 2, 1)
         x = F.dropout2d(x, 0.5, training=self.training)
         # x = x.permute(0, 2, 1)
-        
+
         x = F.max_pool1d(x, kernel_size=5, stride=5)
         x = F.relu(self.bn2(self.conv1(x)))
         x1 = self.mffm_block2(x)
